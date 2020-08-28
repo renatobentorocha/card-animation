70448f7eae1a061907c73dd036926af31500f240
Renato Rocha <renatob.rocha@gmail.com>
2020-08-28 00:10:27 -0300

    Start animation

--- App.tsx
+++ App.tsx
@@ -1,58 +1,106 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useRef } from 'react';
-import { StyleSheet, View } from 'react-native';
-import Animated, { event } from 'react-native-reanimated';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import Animated, {
+  event,
+  abs,
+  block,
+  debug,
+  set,
+  min,
+  useCode,
+  onChange,
+  cond,
+  eq,
+} from 'react-native-reanimated';
 import {
   ScrollView,
   TapGestureHandlerStateChangeEvent,
   PanGestureHandler,
+  LongPressGestureHandler,
+  LongPressGestureHandlerGestureEvent,
+  LongPressGestureHandlerStateChangeEvent,
+  State,
 } from 'react-native-gesture-handler';
 import Card, { CARD_DIMENSIONS } from './src/components/Card';
 
+const { width } = Dimensions.get('screen');
+
 const TOTAL_OF_CARDS = [1, 2, 3, 4];
+
+const CARD_CENTER = {
+  x: CARD_DIMENSIONS.width / 2,
+  y: CARD_DIMENSIONS.height / 2,
+};
 
 export default function App() {
   const translateYs = useRef(
     TOTAL_OF_CARDS.map(() => new Animated.Value<number>())
   ).current;
 
+  const gestureState = useRef(new Animated.Value<State>(State.UNDETERMINED))
+    .current;
+
+  const absY = useRef(new Animated.Value<number>(0)).current;
+
+  const origin = useRef({
+    x: new Animated.Value<number>(0),
+    y: new Animated.Value<number>(0),
+  }).current;
+
   const scrollRef = useRef();
   const scrollPan = useRef();
+
+  useCode(() => onChange(absY, debug('absY', absY)), []);
+
   return (
     <View style={styles.container}>
       <ScrollView
-        ref={scrollRef}
-        simultaneousHandlers={scrollPan}
         horizontal
         showsHorizontalScrollIndicator={false}
         pagingEnabled
         contentContainerStyle={styles.scroll}
       >
         {[1, 2, 3, 4].map((_, index) => (
-          <PanGestureHandler
-            ref={scrollPan}
-            simultaneousHandlers={scrollRef}
+          <LongPressGestureHandler
             key={index.toString()}
-            onGestureEvent={event<TapGestureHandlerStateChangeEvent>([
-              { nativeEvent: { y: translateYs[index] } },
+            onGestureEvent={event<LongPressGestureHandlerGestureEvent>([
+              {
+                nativeEvent: ({ y, absoluteX, absoluteY, state }) =>
+                  block([set(translateYs[index], y), set(gestureState, state)]),
+              },
             ])}
-            onHandlerStateChange={event<TapGestureHandlerStateChangeEvent>([
-              { nativeEvent: { y: translateYs[index] } },
+            onHandlerStateChange={event<
+              LongPressGestureHandlerStateChangeEvent
+            >([
+              {
+                nativeEvent: ({ y, absoluteX, absoluteY, state, handlerTag }) =>
+                  cond(
+                    eq(state, State.ACTIVE),
+                    set(translateYs[index], 1),
+                    cond(eq(translateYs[index], 1), set(translateYs[index], 0))
+                  ),
+                // block([
+                //     set(translateYs[index], y),
+                //     set(gestureState, state),
+                //     set(absY, absoluteY),
+                //     debug('translateYs[index]', translateYs[index]),
+                //   ]),
+              },
             ])}
           >
             <Animated.View style={{}}>
               <Card
                 style={{
                   transform: [
-                    { translateY: translateYs[index] },
                     { translateY: (-CARD_DIMENSIONS.height / 2) * 1.5 },
                     { rotate: '90deg' },
-                    { scale: 1.5 },
+                    { scale: cond(translateYs[index], 1.6, 1.4) },
                   ],
                 }}
               />
             </Animated.View>
-          </PanGestureHandler>
+          </LongPressGestureHandler>
         ))}
       </ScrollView>
       <StatusBar style="auto" />
