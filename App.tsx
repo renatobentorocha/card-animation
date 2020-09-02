a715c4f74a72e29c0a14575a50782eb4444a1f75
Renato Rocha <renatob.rocha@gmail.com>
2020-09-02 01:35:40 -0300

    Start animation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useRef } from 'react';
-import { StyleSheet, View, Dimensions, LayoutChangeEvent } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  LayoutChangeEvent,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+} from 'react-native';
 import Animated, {
   event,
   abs,
@@ -23,17 +30,18 @@ import Animated, {
   Extrapolate,
   concat,
   multiply,
+  sub,
 } from 'react-native-reanimated';
 import {
-  ScrollView,
   LongPressGestureHandler,
-  LongPressGestureHandlerGestureEvent,
   LongPressGestureHandlerStateChangeEvent,
   State,
 } from 'react-native-gesture-handler';
 import Card, { CARD_DIMENSIONS } from './src/components/Card';
 
 const { width } = Dimensions.get('screen');
+
+const CENTER_X = width / 2;
 
 const TOTAL_OF_CARDS = [1, 2, 3, 4];
 
@@ -68,6 +76,17 @@ const runProgress = (clock: Clock, intialPosition: number, toValue: number) => {
   ]);
 };
 
+const position = (
+  value: Animated.Adaptable<number>,
+  scrollX: Animated.Value<number>
+) =>
+  block([
+    debug('sub(value)', sub(value, 0)),
+    debug('sub(scrollX)', sub(scrollX, 0)),
+    debug('sub(value, scrollX)', sub(value, scrollX)),
+    0,
+  ]);
+
 export default function App() {
   const longPressed = useRef(
     TOTAL_OF_CARDS.map(() => new Animated.Value<number>(0))
@@ -81,15 +100,29 @@ export default function App() {
     TOTAL_OF_CARDS.map(() => new Animated.Value<number>(0))
   ).current;
 
+  const scrollX = useRef(new Animated.Value<number>(0)).current;
+
+  const initialPositionX: number[] = TOTAL_OF_CARDS.map(() => 0);
+
   const clock = useRef(TOTAL_OF_CARDS.map(() => new Animated.Clock())).current;
+  const translateXclock = useRef(TOTAL_OF_CARDS.map(() => new Animated.Clock()))
+    .current;
+
+  const onScroll = event<NativeSyntheticEvent<NativeScrollEvent>>([
+    { nativeEvent: { contentOffset: { x: scrollX } } },
+  ]);
+
+  useCode(() => onChange(scrollX, debug('scrollX', scrollX)), []);
 
   return (
     <View style={styles.container}>
-      <ScrollView
+      <Animated.ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
         pagingEnabled
         contentContainerStyle={styles.scroll}
+        scrollEventThrottle={1}
+        onScroll={onScroll}
       >
         {[1, 2, 3, 4].map((_, index) => {
           const rotate = interpolate(translateY[index], {
@@ -98,62 +131,70 @@ export default function App() {
             extrapolate: Extrapolate.CLAMP,
           });
 
+          position(initialPositionX[index], scrollX);
+
           return (
-            <LongPressGestureHandler
+            <Animated.View
               key={index.toString()}
-              onHandlerStateChange={event<
-                LongPressGestureHandlerStateChangeEvent
-              >([
-                {
-                  nativeEvent: ({ state, oldState }) =>
-                    block([
-                      cond(
-                        clockRunning(clock[index]),
-                        set(
-                          translateY[index],
-                          runProgress(clock[index], 0, -200)
-                        )
-                      ),
-                      cond(
-                        clockRunning(clock[index]),
-                        set(
-                          translateX[index],
-                          runProgress(
-                            clock[index],
-                            0,
-                            -index * (CARD_DIMENSIONS.width / 2)
-                          )
-                        )
-                      ),
-                      cond(eq(state, State.ACTIVE), set(longPressed[index], 1)),
-                      cond(eq(oldState, State.ACTIVE), [
-                        startClock(clock[index]),
-                        // set(longPressed[index], 0),
-                      ]),
-                    ]),
-                },
-              ])}
+              onLayout={(event: LayoutChangeEvent) =>
+                (initialPositionX[index] = event.nativeEvent.layout.x)
+              }
             >
-              <Animated.View style={{}}>
-                <Card
-                  onLayout={(event: LayoutChangeEvent) =>
-                    console.log(event.nativeEvent.layout)
-                  }
-                  style={{
-                    transform: [
-                      { translateX: translateX[index] },
-                      { translateY: translateY[index] },
-                      { translateY: (-CARD_DIMENSIONS.height / 2) * 1.5 },
-                      { rotate: concat(rotate, 'deg') },
-                      { scale: cond(longPressed[index], 1.2, 1.4) },
-                    ],
-                  }}
-                />
-              </Animated.View>
-            </LongPressGestureHandler>
+              <LongPressGestureHandler
+                onHandlerStateChange={event<
+                  LongPressGestureHandlerStateChangeEvent
+                >([
+                  {
+                    nativeEvent: ({ state, oldState }) =>
+                      block([
+                        cond(
+                          clockRunning(clock[index]),
+                          set(
+                            translateY[index],
+                            runProgress(clock[index], 0, -200)
+                          )
+                        ),
+                        cond(
+                          clockRunning(translateXclock[index]),
+                          set(
+                            translateX[index],
+                            runProgress(translateXclock[index], 0, -CENTER_X)
+                          )
+                        ),
+                        cond(
+                          eq(state, State.ACTIVE),
+                          set(longPressed[index], 1)
+                        ),
+                        cond(eq(oldState, State.ACTIVE), [
+                          startClock(clock[index]),
+                          startClock(translateXclock[index]),
+                          // set(longPressed[index], 0),
+                        ]),
+                      ]),
+                  },
+                ])}
+              >
+                <Animated.View style={{}}>
+                  <Card
+                    style={[
+                      {
+                        transform: [
+                          { translateX: translateX[index] },
+                          { translateY: translateY[index] },
+                          { translateX: multiply(translateX[index], -1) },
+                          { translateY: (-CARD_DIMENSIONS.height / 2) * 1.5 },
+                          { rotate: concat(rotate, 'deg') },
+                          { scale: cond(longPressed[index], 1.2, 1.4) },
+                        ],
+                      },
+                    ]}
+                  />
+                </Animated.View>
+              </LongPressGestureHandler>
+            </Animated.View>
           );
         })}
-      </ScrollView>
+      </Animated.ScrollView>
       <StatusBar style="auto" />
     </View>
   );
